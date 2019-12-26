202010bfbdab5a205d683696b151b7571c36a429
RedstoneParadox <spinjitzu00@gmail.com>
2019-12-26 14:47:45 -0800

    Dead bushes can now be placed on cracked ground

---
+++ src/main/java/io/github/redstoneparadox/mixin/MixinDeadBushBlock.java
@@ -0,0 +1,22 @@
+package io.github.redstoneparadox.mixin;
+
+import io.github.redstoneparadox.block.JourniaBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DeadBushBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DeadBushBlock.class)
+public class MixinDeadBushBlock {
+
+    @Inject(method = "canPlantOnTop", at = @At("RETURN"), cancellable = true)
+    private void canPlaceOn(BlockState floor, BlockView view, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        Block block = floor.getBlock();
+        cir.setReturnValue(cir.getReturnValueZ() || block == JourniaBlocks.INSTANCE.getCRACKED_GROUND());
+    }
+}
