7f4b6ff07669fdd6e6793a967c8f971fa5e4a51f
RedstoneParadox <spinjitzu00@gmail.com>
2019-12-26 11:50:45 -0800

    Setup biomes singleton

---
+++ src/main/java/io/github/redstoneparadox/world/biome/JourniaBiomes.kt
@@ -0,0 +1,46 @@
+package io.github.redstoneparadox.world.biome
+
+import net.fabricmc.fabric.api.biomes.v1.FabricBiomes
+import net.fabricmc.fabric.api.biomes.v1.OverworldBiomes
+import net.fabricmc.fabric.api.biomes.v1.OverworldClimate
+import net.minecraft.util.registry.Registry
+import net.minecraft.world.biome.Biome
+
+object JourniaBiomes {
+
+    fun registerAll() {
+
+    }
+
+    fun register(id: String, biome: Biome) {
+        Registry.register(Registry.BIOME, "journia:$id", biome)
+    }
+
+    fun spawnBiome(biome: Biome) {
+        FabricBiomes.addSpawnBiome(biome)
+    }
+
+    fun continentalBiome(biome: Biome, climate: OverworldClimate, weight: Double) {
+        OverworldBiomes.addContinentalBiome(biome, climate, weight)
+    }
+
+    fun hillsBiome(parent: Biome, hills: Biome, weight: Double) {
+        OverworldBiomes.addHillsBiome(parent, hills, weight)
+    }
+
+    fun shoreBiome(parent: Biome, shore: Biome, weight: Double) {
+        OverworldBiomes.addShoreBiome(parent, shore, weight)
+    }
+
+    fun edgeBiome(parent: Biome, edge: Biome, weight: Double) {
+        OverworldBiomes.addEdgeBiome(parent, edge, weight)
+    }
+
+    fun variantBiome(replaced: Biome, variant: Biome, chance: Double, vararg climates: OverworldClimate) {
+        OverworldBiomes.addBiomeVariant(replaced, variant, chance, *climates)
+    }
+
+    fun riverBiome(parent: Biome, river: Biome) {
+        OverworldBiomes.setRiverBiome(parent, river)
+    }
+}
