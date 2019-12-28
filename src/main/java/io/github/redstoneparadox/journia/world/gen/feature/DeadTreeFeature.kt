6a8f59c2b97719fbfa062e4bccd32eaaf95deb82
RedstoneParadox <spinjitzu00@gmail.com>
2019-12-27 20:39:19 -0800

    Dead trees can now spawn fallen over

--- src/main/java/io/github/redstoneparadox/journia/world/gen/feature/DeadTreeFeature.kt
+++ src/main/java/io/github/redstoneparadox/journia/world/gen/feature/DeadTreeFeature.kt
@@ -2,8 +2,12 @@ package io.github.redstoneparadox.journia.world.gen.feature
 
 import com.mojang.datafixers.Dynamic
 import io.github.redstoneparadox.journia.block.JourniaBlocks
+import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
+import net.minecraft.block.PillarBlock
+import net.minecraft.state.property.Properties
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.world.IWorld
 import net.minecraft.world.gen.chunk.ChunkGenerator
 import net.minecraft.world.gen.chunk.ChunkGeneratorConfig
@@ -18,9 +22,41 @@ class DeadTreeFeature(configDeserializer: Function<Dynamic<*>, out DeadTreeFeatu
         val ground = world.getBlockState(pos.down())
 
         if (ground == JourniaBlocks.CRACKED_GROUND.defaultState || ground == Blocks.COARSE_DIRT.defaultState) {
-            for (i in 0 until height) world.setBlockState(pos.up(i), trunk, 0)
+            val fallen = generateFallen(random, world, pos, trunk, height)
+            if (!fallen) for (i in 0 until height) world.setBlockState(pos.up(i), trunk, 0)
         }
 
         return true
+    }
+
+    private fun generateFallen(random: Random, world: IWorld, pos: BlockPos, trunk: BlockState, height: Int): Boolean {
+        if (random.nextFloat() <= 0.3) {
+            val directions = listOf(Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST).filter { direction ->
+                var fallenPos = pos
+                for (i in 1..(height + 1)) {
+                    fallenPos = fallenPos.offset(direction)
+                    if (!world.testBlockState(fallenPos) {it.isAir}) break
+                    return@filter true
+                }
+                false
+            }
+
+            if (directions.isEmpty()) return false
+
+            val index = random.nextInt(directions.size)
+            val direction = directions[index]
+            var fallenPos = pos.offset(direction)
+
+            world.setBlockState(pos, trunk, 0)
+
+            for (i in 1 until height) {
+                fallenPos = fallenPos.offset(direction)
+                val log = if (trunk.block is PillarBlock) trunk.with(Properties.AXIS, direction.axis) else trunk
+                world.setBlockState(fallenPos, log, 0)
+            }
+
+            return true
+        }
+        return false
     }
 }
