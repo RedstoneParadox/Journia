6f970b89bd351d5783129e0fd6ba8e8a7b1823c1
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-18 16:38:29 -0700

    Further refined azure biome concept

--- src/main/java/io/github/redstoneparadox/journia/world/biome/AzureBiomes.kt
+++ src/main/java/io/github/redstoneparadox/journia/world/biome/AzureBiomes.kt
@@ -15,22 +15,26 @@ object AzureBiomes {
     init {
         val template = TerraformBiome.Template(
             TerraformBiome.builder()
-                .configureSurfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_SAND_UNDERWATER_CONFIG)
+                .configureSurfaceBuilder(SurfaceBuilder.DEFAULT, JourniaSurfaceBuilders.AZURE_CONFIG)
                 .temperature(0.95f)
-                .downfall(0.0f)
+                .downfall(0.9f)
                 .precipitation(Biome.Precipitation.RAIN)
                 .waterColor(0x47d1d1)
                 .waterFogColor(0x2eb8b8)
+                .grassColor(0x00cc00)
+                .foliageColor(0x00cc00)
                 .category(Biome.Category.EXTREME_HILLS)
                 .addCustomFeature(
                     GenerationStep.Feature.RAW_GENERATION,
                     JourniaFeatures.BLOCK_BANDS.configure(
                         BlockBandsFeatureConfig(
                             listOf(
-                                BlockBandsFeatureConfig.BlockBand(Blocks.BLUE_TERRACOTTA.defaultState, 2, 3),
-                                BlockBandsFeatureConfig.BlockBand(Blocks.BLUE_TERRACOTTA.defaultState, 5, 1)
+                                BlockBandsFeatureConfig.BlockBand(Blocks.CYAN_CONCRETE.defaultState, 2, 3),
+                                BlockBandsFeatureConfig.BlockBand(Blocks.CYAN_CONCRETE.defaultState, 5, 1),
+                                BlockBandsFeatureConfig.BlockBand(Blocks.LIGHT_BLUE_TERRACOTTA.defaultState, 3, 2),
+                                BlockBandsFeatureConfig.BlockBand(Blocks.BLUE_TERRACOTTA.defaultState, 3, 1)
                             ),
-                            3,
+                            4,
                             7
                         )
                     )
@@ -38,8 +42,8 @@ object AzureBiomes {
         )
 
         AZURE = template.builder()
-            .depth(0.125f)
-            .scale(1.0f)
+            .depth(0.3f)
+            .scale(0.75f)
             .build()
     }
 
