b0aea1848eb027bbb7401e9ba4858ba9f814b4aa
RedstoneParadox <spinjitzu00@gmail.com>
2020-07-04 11:14:30 -0700

    Removed old API

--- src/main/java/io/github/redstoneparadox/journia/block/JourniaSaplingBlock.kt
+++ src/main/java/io/github/redstoneparadox/journia/block/JourniaSaplingBlock.kt
@@ -1,9 +1,9 @@
 package io.github.redstoneparadox.journia.block
 
-import net.fabricmc.fabric.api.block.FabricBlockSettings
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Blocks
 import net.minecraft.block.SaplingBlock
 import net.minecraft.block.sapling.SaplingGenerator
 
-class JourniaSaplingBlock(generator: SaplingGenerator): SaplingBlock(generator, FabricBlockSettings.copy(Blocks.OAK_SAPLING).build()) {
+class JourniaSaplingBlock(generator: SaplingGenerator): SaplingBlock(generator, FabricBlockSettings.copy(Blocks.OAK_SAPLING)) {
 }
