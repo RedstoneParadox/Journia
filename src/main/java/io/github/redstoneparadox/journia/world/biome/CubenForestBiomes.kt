11d0509fbbe0468d9660cfb1fcbabb1dd476d45f
RedstoneParadox <spinjitzu00@gmail.com>
2020-10-17 19:23:42 -0700

    Cuben Forest biomes

--- src/main/java/io/github/redstoneparadox/journia/world/biome/CubenForestBiomes.kt
+++ src/main/java/io/github/redstoneparadox/journia/world/biome/CubenForestBiomes.kt
@@ -1,53 +1,53 @@
 package io.github.redstoneparadox.journia.world.biome
 
-import com.terraformersmc.terraform.biome.builder.DefaultFeature.*
-import com.terraformersmc.terraform.biome.builder.TerraformBiome
+import com.terraformersmc.terraform.biomebuilder.TerraformBiomeBuilder
 import io.github.redstoneparadox.journia.config.BiomesConfig
 import io.github.redstoneparadox.journia.world.gen.feature.JourniaFeatures
-import net.fabricmc.fabric.api.biomes.v1.OverworldClimate
 import net.minecraft.world.biome.Biome
 import net.minecraft.world.gen.feature.*
+import com.terraformersmc.terraform.biomebuilder.DefaultFeature.*
+import net.fabricmc.fabric.api.biome.v1.OverworldBiomes
+import net.fabricmc.fabric.api.biome.v1.OverworldClimate
+import net.minecraft.world.biome.BiomeEffects
 import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder
 
 
 object CubenForestBiomes {
-    val CUBEN_FOREST: Biome
-    val CUBEN_FOREST_HILLS: Biome
-
-    init {
-        val template = TerraformBiome.Template(
-            TerraformBiome.builder()
+   fun register() {
+        if (BiomesConfig.CubenForest.enabled) {
+            val template = TerraformBiomeBuilder.create()
                 .configureSurfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_SAND_UNDERWATER_CONFIG)
                 .temperature(0.7F).downfall(0.8F)
                 .precipitation(Biome.Precipitation.RAIN)
                 .category(Biome.Category.FOREST)
                 .addTreeFeature(Feature.TREE.configure(JourniaFeatures.cubenTreeConfig()), 5)
                 .addDefaultSpawnEntries()
-                .addStructureFeature<MineshaftFeatureConfig>(MineshaftFeature.MINESHAFT.configure(MineshaftFeatureConfig(0.004, MineshaftFeature.Type.NORMAL)))
-                .addDefaultFeatures(LAND_CARVERS, STRUCTURES, LAKES, DUNGEONS, PLAINS_TALL_GRASS, MINEABLES, ORES, DISKS,
+                .addStructureFeature<MineshaftFeatureConfig>(MineshaftFeature.MINESHAFT.configure(MineshaftFeatureConfig(0.004f, MineshaftFeature.Type.NORMAL)))
+                .addDefaultFeatures(LAND_CARVERS, /*STRUCTURES,*/ LAKES, DUNGEONS, PLAINS_TALL_GRASS, MINEABLES, ORES, DISKS,
                     PLAINS_FEATURES, DEFAULT_MUSHROOMS, FOREST_FLOWERS, FOREST_GRASS, SPRINGS, FROZEN_TOP_LAYER)
                 .addStructureFeature<DefaultFeatureConfig>(StrongholdFeature.STRONGHOLD.configure(FeatureConfig.DEFAULT))
-                .waterColor(4159204)
-                .waterFogColor(329011)
-        )
+                .effects(
+                    BiomeEffects.Builder()
+                        .waterColor(4159204)
+                        .waterFogColor(329011)
+                )
 
-        CUBEN_FOREST = template.builder()
-            .depth(0.1f)
-            .scale(0.2f)
-            .build()
+            JourniaBiomes.CUBEN_FOREST = JourniaBiomes.register("cuben_forest",
+                TerraformBiomeBuilder.create(template)
+                    .depth(0.1f)
+                    .scale(0.2f)
+                    .build()
+            )
 
-        CUBEN_FOREST_HILLS = template.builder()
-            .depth(0.45f)
-            .scale(0.3f)
-            .build()
-    }
+            JourniaBiomes.CUBEN_FOREST_HILLS = JourniaBiomes.register("cuben_forest_hills",
+                TerraformBiomeBuilder.create(template)
+                    .depth(0.45f)
+                    .scale(0.3f)
+                    .build()
+            )
 
-    fun register() {
-        if (BiomesConfig.CubenForest.enabled) {
-            JourniaBiomes.register("cuben_forest", CUBEN_FOREST)
-            JourniaBiomes.register("cuben_forest_hills", CUBEN_FOREST_HILLS)
-            JourniaBiomes.continentalBiome(CUBEN_FOREST, OverworldClimate.TEMPERATE, BiomesConfig.CubenForest.weight)
-            JourniaBiomes.hillsBiome(CUBEN_FOREST, CUBEN_FOREST_HILLS, BiomesConfig.CubenForest.hills_chance)
+            OverworldBiomes.addContinentalBiome(JourniaBiomes.CUBEN_FOREST, OverworldClimate.TEMPERATE, BiomesConfig.CubenForest.weight)
+            OverworldBiomes.addHillsBiome(JourniaBiomes.CUBEN_FOREST, JourniaBiomes.CUBEN_FOREST_HILLS, BiomesConfig.CubenForest.hills_chance)
         }
     }
 }
--- src/main/java/io/github/redstoneparadox/journia/world/biome/JourniaBiomes.kt
+++ src/main/java/io/github/redstoneparadox/journia/world/biome/JourniaBiomes.kt
@@ -1,23 +1,10 @@
 package io.github.redstoneparadox.journia.world.biome
 
-import com.terraformersmc.terraform.biome.builder.TerraformBiome
-import io.github.redstoneparadox.journia.config.BiomesConfig
-import io.github.redstoneparadox.journia.world.gen.decorator.JourniaDecorators
-import io.github.redstoneparadox.journia.world.gen.feature.JourniaFeatures
-import io.github.redstoneparadox.journia.world.gen.feature.RockFormationFeatureConfig
-import net.fabricmc.fabric.api.biomes.v1.FabricBiomes
-import net.fabricmc.fabric.api.biomes.v1.OverworldBiomes
-import net.fabricmc.fabric.api.biomes.v1.OverworldClimate
-import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.BuiltinRegistries
 import net.minecraft.util.registry.Registry
 import net.minecraft.util.registry.RegistryKey
 import net.minecraft.world.biome.Biome
-import net.minecraft.world.biome.Biomes
-import net.minecraft.world.gen.GenerationStep
-import net.minecraft.world.gen.decorator.ChanceDecoratorConfig
-import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder
 
 object JourniaBiomes {
     lateinit var BANDED_MOUNTAINS: RegistryKey<Biome>
@@ -25,6 +12,8 @@ object JourniaBiomes {
     lateinit var BANDED_MOUNTAINS_RIVER: RegistryKey<Biome>
     lateinit var BANDED_SHORE: RegistryKey<Biome>
 
+    lateinit var CUBEN_FOREST: RegistryKey<Biome>
+    lateinit var CUBEN_FOREST_HILLS: RegistryKey<Biome>
 
     fun registerAll() {
         ShoreBiomes.register()
@@ -40,13 +29,5 @@ object JourniaBiomes {
         BuiltinRegistries.add(BuiltinRegistries.BIOME, id, biome)
 
         return RegistryKey.of(Registry.BIOME_KEY, id)
-    }
-
-    fun continentalBiome(biome: Biome, climate: OverworldClimate, weight: Double) {
-        OverworldBiomes.addContinentalBiome(biome, climate, weight)
-    }
-
-    fun hillsBiome(parent: Biome, hills: Biome, weight: Double) {
-        OverworldBiomes.addHillsBiome(parent, hills, weight)
     }
 }
