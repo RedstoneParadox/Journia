8496e80fba869f4449fe1588da4c25513084fac6
RedstoneParadox <spinjitzu00@gmail.com>
2020-10-18 10:06:43 -0700

    BiomeBuilderUtil is no longer needed

--- src/main/java/io/github/redstoneparadox/journia/util/BiomeBuilderUtil.kt
+++
@@ -1,7 +0,0 @@
-package io.github.redstoneparadox.journia.util
-
-import com.terraformersmc.terraform.biome.builder.TerraformBiome
-
-fun TerraformBiome.Builder.skyFogColor(color: Int): TerraformBiome.Builder {
-    return this.fogColor(color) as TerraformBiome.Builder
-}
