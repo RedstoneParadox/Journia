c7ff715a06212b06b9e718120adb16448129763d
RedstoneParadox <spinjitzu00@gmail.com>
2020-10-17 19:27:54 -0700

    Salt Flats biomes

--- src/main/java/io/github/redstoneparadox/journia/world/biome/SaltFlatsBiomes.kt
+++ src/main/java/io/github/redstoneparadox/journia/world/biome/SaltFlatsBiomes.kt
@@ -1,6 +1,7 @@
 package io.github.redstoneparadox.journia.world.biome
 
 import com.terraformersmc.terraform.biome.builder.TerraformBiome
+import com.terraformersmc.terraform.biomebuilder.TerraformBiomeBuilder
 import io.github.redstoneparadox.journia.world.gen.surfacebuilder.JourniaSurfaceBuilders
 import net.minecraft.world.biome.Biome
 import net.minecraft.world.biome.BiomeEffects
@@ -11,27 +12,24 @@ object SaltFlatsBiomes {
     val SALT_FLATS: Biome
 
     init {
-        val template = TerraformBiome.Template(TerraformBiome.builder()
+        val template = TerraformBiomeBuilder.create()
             .configureSurfaceBuilder(SurfaceBuilder.DEFAULT, JourniaSurfaceBuilders.SALT_FLATS_CONFIG)
             .temperature(3.0f)
             .downfall(0.0f)
             .precipitation(Biome.Precipitation.NONE)
-            .waterColor(0x32a598)
-            .waterFogColor(329011)
+            .effects(createEffects())
             .category(Biome.Category.DESERT)
-        )
 
-        SALT_FLATS = template.builder()
+        SALT_FLATS = TerraformBiomeBuilder.create(template)
             .depth(0.01f)
             .scale(-0.075f)
             .build()
     }
 
-    fun createEffects(): BiomeEffects {
+    private fun createEffects(): BiomeEffects.Builder {
         return BiomeEffects.Builder()
             .waterColor(0x32a598)
             .waterFogColor(329011)
             .fogColor(12638463)
-            .build()
     }
 }
